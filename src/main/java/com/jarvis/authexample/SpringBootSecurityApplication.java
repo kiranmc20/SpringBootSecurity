4588b91c6a8c61231ad7efe623555c5e9f4edc2a
Kiran <kiranmc20@gmail.com>
2025-01-28 20:00:07 +0530

    Jwt authentication changes(generate JWT token and validate JWT token)

---
+++ src/main/java/com/jarvis/authexample/SpringBootSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.jarvis.authexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityApplication.class, args);
+	}
+
+}
